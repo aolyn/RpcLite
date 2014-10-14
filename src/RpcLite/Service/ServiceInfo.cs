0a46d7e9be5b2672bfc3b2c2ae82adfd088cea24
Chris <chrishwang@live.cn>
2014-10-14 16:13:41 +0800

    use single assembly

---
+++ src/RpcLite/Service/ServiceInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RpcLite
+{
+	public class ServiceInfo
+	{
+		public string Path { get; set; }
+		public Type Type { get; set; }
+		public string Name { get; set; }
+
+		public override string ToString()
+		{
+			return string.Format("{0}, {1}, {2}", Name, Path, Type);
+		}
+	}
+}
