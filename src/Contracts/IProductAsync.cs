46881b82a7ec4b27753de61d73b937b92cf06831
Chris <chrishwang@live.cn>
2014-10-28 15:44:10 +0800

    add async handler

---
+++ src/Contracts/IProductAsync.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Model;
+
+namespace Contracts
+{
+	public interface IProductAsync
+	{
+		IAsyncResult BeginAdd(Product product, AsyncCallback cb, object state);
+		int EndAdd(IAsyncResult ar);
+
+		//int AddProduct(int id, Product product);
+		//int AddProduct(int id, Product product, int a, int b, int c, int d);
+		//void Delete(int id);
+		//Product[] Get();
+		//Product GetById(int id);
+		//Product Get(int id);
+	}
+}
