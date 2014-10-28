3f626cd4676cb010a795fc440d5567853ee03e11
Chris <chrishwang@live.cn>
2014-10-28 15:44:10 +0800

    add async handler

---
+++ src/RpcLite/Service/RpcAsyncHandler.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using Newtonsoft.Json;
+using RpcLite.Config;
+using RpcLite.Formatters;
+using RpcLite.Service;
+
+namespace RpcLite
+{
+	public class RpcAsyncHandler : IHttpAsyncHandler
+	{
+		#region Sync
+		public void ProcessRequest(HttpContext context)
+		{
+			var request = context.Request;
+			var response = context.Response;
+
+			try
+			{
+				//get formatter from content type
+				IFormatter formatter;
+				if (!string.IsNullOrEmpty(request.ContentType))
+				{
+					formatter = GlobalConfig.Formaters.FirstOrDefault(it => it.SupportMimes.Contains(request.ContentType));
+					if (formatter == null)
+						throw new ConfigException("Not Supported MIME: " + request.ContentType);
+
+					response.ContentType = request.ContentType;
+				}
+				else
+				{
+					if (GlobalConfig.Formaters.Count == 0)
+						throw new ConfigException("Configuration error: no formatters.");
+
+					formatter = GlobalConfig.Formaters[0];
+					if (formatter.SupportMimes.Count == 0)
+						throw new ConfigException("Configuration error: formatter " + formatter.GetType() + " has no support MIME");
+
+					response.ContentType = formatter.SupportMimes[0];
+				}
+
+				ProcessRequest(request, response, formatter);
+			}
+			catch (ThreadAbortException)
+			{
+			}
+			catch (Exception ex)
+			{
+				//by default send Exception data to client use Json Format
+				var resultJson = JsonConvert.SerializeObject(ex);
+				response.Write(resultJson);
+				response.End();
+			}
+		}
+
+		private static void ProcessRequest(HttpRequest request, HttpResponse response, IFormatter formatter)
+		{
+			if (request == null) throw new ArgumentNullException("request");
+
+			var requestPath = request.AppRelativeCurrentExecutionFilePath;
+
+			if (string.IsNullOrWhiteSpace(requestPath))
+				throw new ArgumentException("request.AppRelativeCurrentExecutionFilePath is null or white space");
+
+			var service = RpcServiceHelper.Services.FirstOrDefault(it =>
+					requestPath.StartsWith(it.Path, StringComparison.OrdinalIgnoreCase));
+
+			if (service == null)
+			{
+				formatter.Serialize(response.OutputStream, new ConfigException("Configuration error service not found"));
+				return;
+			}
+
+			try
+			{
+				var actionName = requestPath.Substring(service.Path.Length);
+				if (string.IsNullOrEmpty(actionName))
+					throw new RequestException("Bad request: not action name");
+
+				var serviceRequest = new ServiceRequest
+				{
+					ActionName = actionName,
+					Formatter = formatter,
+					InputStream = request.InputStream,
+					ServiceType = service.Type,
+				};
+
+				try
+				{
+					var result = RpcProcessor.ProcessRequest(serviceRequest);
+					formatter.Serialize(response.OutputStream, result);
+				}
+				catch (Exception ex)
+				{
+					throw new ProcessRequestException(ex.Message, ex);
+				}
+			}
+			catch (Exception ex)
+			{
+				formatter.Serialize(response.OutputStream, ex);
+			}
+		}
+
+		public bool IsReusable
+		{
+			get
+			{
+				return true;
+			}
+		}
+
+		#endregion
+
+		public IAsyncResult BeginProcessRequest(HttpContext context, AsyncCallback cb, object extraData)
+		{
+			var request = context.Request;
+			var response = context.Response;
+
+			try
+			{
+				//get formatter from content type
+				IFormatter formatter = GetFormatter(request.ContentType);
+				if (formatter != null)
+					response.ContentType = request.ContentType;
+
+				return ProcessRequestAsync(request.InputStream, response.OutputStream, context, request.AppRelativeCurrentExecutionFilePath, formatter, cb);
+			}
+			catch (ThreadAbortException)
+			{
+				return new ServiceAsyncResult
+				{
+					AsyncState = extraData,
+					IsCompleted = true,
+					CompletedSynchronously = true,
+					AsyncWaitHandle = null,
+					HttpContext = HttpContext.Current,
+				};
+			}
+			catch (Exception ex)
+			{
+				//by default send Exception data to client use Json Format
+				var resultJson = JsonConvert.SerializeObject(ex);
+				response.Write(resultJson);
+				response.End();
+			}
+
+			return null;
+		}
+
+		private static IFormatter GetFormatter(string contentType)
+		{
+			IFormatter formatter;
+			if (!string.IsNullOrEmpty(contentType))
+			{
+				formatter = GlobalConfig.Formaters.FirstOrDefault(it => it.SupportMimes.Contains(contentType));
+				if (formatter == null)
+					throw new ConfigException("Not Supported MIME: " + contentType);
+			}
+			else
+			{
+				if (GlobalConfig.Formaters.Count == 0)
+					throw new ConfigException("Configuration error: no formatters.");
+
+				formatter = GlobalConfig.Formaters[0];
+				if (formatter.SupportMimes.Count == 0)
+					throw new ConfigException("Configuration error: formatter " + formatter.GetType() + " has no support MIME");
+			}
+			return formatter;
+		}
+
+		private static IAsyncResult ProcessRequestAsync(Stream requestStream, Stream responseStream, object requestContext, string requestPath, IFormatter formatter, AsyncCallback cb)
+		{
+			if (requestStream == null) throw new ArgumentNullException("request");
+
+			//var requestPath = request.AppRelativeCurrentExecutionFilePath;
+
+			if (string.IsNullOrWhiteSpace(requestPath))
+				throw new ArgumentException("request.AppRelativeCurrentExecutionFilePath is null or white space");
+
+			var service = RpcServiceHelper.Services.FirstOrDefault(it =>
+					requestPath.StartsWith(it.Path, StringComparison.OrdinalIgnoreCase));
+
+			//if (service == null)
+			//{
+			//	formatter.Serialize(responseStream, new ConfigException("Configuration error service not found"));
+			//	return;
+			//}
+
+			try
+			{
+				var actionName = requestPath.Substring(service.Path.Length);
+				if (string.IsNullOrEmpty(actionName))
+					throw new RequestException("Bad request: not action name");
+
+				var serviceRequest = new ServiceRequest
+				{
+					ActionName = actionName,
+					Formatter = formatter,
+					InputStream = requestStream,
+					ServiceType = service.Type,
+				};
+
+				try
+				{
+					var result = RpcProcessor.BeginProcessRequest(serviceRequest, cb, requestContext);
+					//formatter.Serialize(responseStream, result);
+					return result;
+				}
+				catch (Exception ex)
+				{
+					throw new ProcessRequestException(ex.Message, ex);
+				}
+			}
+			catch (Exception ex)
+			{
+				//formatter.Serialize(responseStream.OutputStream, ex);
+				var result = new ServiceAsyncResult
+				{
+					HttpContext = requestContext,
+					AsyncWaitHandle = null,
+					CompletedSynchronously = true,
+					IsCompleted = true,
+					AsyncState = null,
+				};
+
+				cb(result);
+				return result;
+			}
+		}
+
+		public void EndProcessRequest(IAsyncResult result)
+		{
+			var ar = result as ServiceAsyncResult;
+			if (ar != null)
+			{
+
+			}
+		}
+	}
+
+	class RpcServiceHelper
+	{
+		public static List<ServiceInfo> Services { get { return RpcLiteConfigSection.Instance.Services; } }
+
+	}
+
+}
---
+++ src/ServiceImpl/ProductAsyncService.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Contracts;
+using Model;
+
+namespace ServiceImpl
+{
+	public class ProductAsyncService : IProductAsync
+	{
+		static readonly List<Product> Products = new List<Product>();
+		static ProductAsyncService()
+		{
+			var rnd = new Random();
+			for (int i = 0; i < 10; i++)
+			{
+				Products.Add(new Product
+				{
+					Id = i,
+					Name = DateTime.Now.ToString(),
+					ListDate = DateTime.Now,
+					Category = "aa",
+					Price = rnd.Next(1000)
+				});
+			}
+		}
+
+		//public Product[] Get()
+		//{
+		//	return Products.ToArray();
+		//}
+
+		//public Product Get(int id)
+		//{
+		//	return new Product { Id = id, Name = DateTime.Now.ToString() };
+		//}
+
+		//public int Add(Product product)
+		//{
+		//	Products.Add(product);
+		//	return product.Id;
+		//}
+
+		//public int AddProduct(int id, Product product)
+		//{
+		//	Products.Add(product);
+		//	return id;
+		//}
+
+		//public void Delete(int id)
+		//{
+		//	//return new Product() { Id = id, Name = DateTime.Now.ToString() };
+		//}
+
+		//public Product GetById(int id)
+		//{
+		//	return Products.FirstOrDefault(it => it.Id == id);
+		//}
+
+		//public int AddProduct(int id, Product product, int a, int b, int c, int d)
+		//{
+		//	throw new NotImplementedException();
+		//}
+
+		public IAsyncResult BeginAdd(Product product, AsyncCallback cb, object state)
+		{
+			return new Action<Product>(Add).BeginInvoke(product, cb, state);
+		}
+
+		int addedProductId = -1;
+		public void Add(Product product)
+		{
+			Products.Add(product);
+			addedProductId = product.Id;
+		}
+
+		public int EndAdd(IAsyncResult ar)
+		{
+			return addedProductId;
+		}
+	}
+}
