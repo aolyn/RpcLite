21b898f2c6542cf59b883390940da64fdc37206c
Chris <chrishwang@live.cn>
2016-05-14 16:38:48 +0800

    for async service only support task, IAsync removed

---
+++ src/RpcLite.TestWeb/TestService1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+
+namespace RpcLite.TestWeb
+{
+	public class TestService1
+	{
+		public string GetDateTimeTimeString()
+		{
+			return DateTime.Now.ToString(CultureInfo.InvariantCulture);
+
+		}
+	}
+}
---
+++ src/RpcLite/content/TestService1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace RpcLiteServiceTest
+{
+	public class TestService1
+	{
+		public string GetDateTimeString()
+		{
+			return DateTime.Now.ToString(CultureInfo.InvariantCulture);
+		}
+	}
+}
