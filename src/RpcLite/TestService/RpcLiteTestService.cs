9b0d1d357cd75450452bceedd6da76bc6305ce95
Chris <chrishwang@live.cn>
2016-05-16 21:15:01 +0800

    add ClientFactory & test service

---
+++ src/RpcLite/TestService/RpcLiteTestService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+
+namespace RpcLite.TestService
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class RpcLiteTestService : IRpcLiteTestService
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		public string GetDateTimeString()
+		{
+			return DateTime.Now.ToString(CultureInfo.InvariantCulture);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		/// <returns></returns>
+		public int Add(int a, int b)
+		{
+			return a + b;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="product"></param>
+		/// <returns></returns>
+		public int AddProduct(Product product)
+		{
+			return product?.Id ?? 0;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns></returns>
+		public Product GetProductById(int id)
+		{
+			return new Product
+			{
+				Id = id,
+				Name = "Access to RpcLite",
+				Price = 50,
+			};
+		}
+
+	}
+
+}
