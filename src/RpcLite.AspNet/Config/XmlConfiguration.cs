cc7f552450a5a78a3c5bd7119ee50790ce497d24
Chris <chrishwang@live.cn>
2016-05-17 19:16:31 +0800

    abstract IConfiguration.cs

---
+++ src/RpcLite.AspNet/Config/XmlConfiguration.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+
+namespace RpcLite.Config
+{
+	public class XmlConfiguration : IConfiguration
+	{
+		private XmlNode _node;
+
+		public XmlConfiguration(XmlNode node)
+		{
+			_node = node;
+		}
+
+		public string this[string key]
+		{
+			get
+			{
+				var attrValue = RpcLiteConfigurationHelper.GetAttribute(key, _node);
+				if (attrValue != null)
+					return attrValue;
+
+				var node = _node[key];
+				return node?.InnerText;
+			}
+
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public IEnumerable<IConfigurationSection> GetChildren()
+		{
+			return _node.ChildNodes
+				.Cast<XmlNode>()
+				.Select(it => new XmlConfigurationSection(it));
+		}
+
+		public IConfigurationSection GetSection(string key)
+		{
+			var node = _node[key];
+			return node == null
+				? null
+				: new XmlConfigurationSection(node);
+		}
+	}
+}
---
+++ src/RpcLite.AspNet/Config/XmlConfigurationSection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+
+namespace RpcLite.Config
+{
+	public class XmlConfigurationSection : IConfigurationSection
+	{
+		private XmlNode _node;
+
+		public XmlConfigurationSection(XmlNode node)
+		{
+			_node = node;
+		}
+
+		public IEnumerable<IConfigurationSection> GetChildren()
+		{
+			return _node.ChildNodes
+				.Cast<XmlNode>()
+				.Select(it => new XmlConfigurationSection(it));
+		}
+
+		public string this[string key]
+		{
+			get { return _node.Name; }
+			set { throw new NotImplementedException(); }
+		}
+
+		public IConfigurationSection GetSection(string key)
+		{
+			var node = _node[key];
+			return node == null
+				? null
+				: new XmlConfigurationSection(node);
+		}
+
+		public string Key { get { return _node.Name; } }
+		public string Path { get; }
+		public string Value { get; set; }
+	}
+}
