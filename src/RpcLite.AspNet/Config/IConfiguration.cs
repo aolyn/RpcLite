8c12bea87c0999ff2c902f5970d13a0d84a34185
Chris <chrishwang@live.cn>
2016-05-17 19:16:31 +0800

    abstract IConfiguration.cs

---
+++ src/RpcLite.AspNet/Config/IConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace RpcLite.Config
+{
+	public interface IConfiguration
+	{
+		//
+		// Summary:
+		//     Gets the immediate descendant configuration sub-sections.
+		//
+		// Returns:
+		//     The configuration sub-sections.
+		IEnumerable<IConfigurationSection> GetChildren();
+
+		//
+		// Summary:
+		//     Gets or sets a configuration value.
+		//
+		// Parameters:
+		//   key:
+		//     The configuration key.
+		//
+		// Returns:
+		//     The configuration value.
+		string this[string key] { get; set; }
+
+		//
+		// Summary:
+		//     Gets a configuration sub-section with the specified key.
+		//
+		// Parameters:
+		//   key:
+		//     The key of the configuration section.
+		//
+		// Returns:
+		//     The Microsoft.Extensions.Configuration.IConfigurationSection.
+		//
+		// Remarks:
+		//     This method will never return null. If no matching sub-section is found with
+		//     the specified key, an empty Microsoft.Extensions.Configuration.IConfigurationSection
+		//     will be returned.
+		IConfigurationSection GetSection(string key);
+	}
+}
---
+++ src/RpcLite.AspNet/Config/IConfigurationSection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RpcLite.Config
+{
+	//
+	// Summary:
+	//     Represents a section of application configuration values.
+	public interface IConfigurationSection : IConfiguration
+	{
+		//
+		// Summary:
+		//     Gets the key this section occupies in its parent.
+		string Key { get; }
+		//
+		// Summary:
+		//     Gets the full path to this section within the Microsoft.Extensions.Configuration.IConfiguration.
+		string Path { get; }
+		//
+		// Summary:
+		//     Gets or sets the section value.
+		string Value { get; set; }
+	}
+}
