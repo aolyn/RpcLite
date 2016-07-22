4e87cc8d6362a15aa5e3ae5a826420ff8a99c3e0
Chris <chrishwang@live.cn>
2016-07-22 14:26:00 +0800

    add https for test site

--- src/ServiceTest/ServiceTest.WebHost/Program.cs
+++ src/ServiceTest/ServiceTest.WebHost/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Hosting;
 
 namespace ServiceTest.WebHost
@@ -7,15 +8,34 @@ namespace ServiceTest.WebHost
 	{
 		public static void Main(string[] args)
 		{
+			//var host = new WebHostBuilder()
+			//	.UseKestrel()
+			//	.UseContentRoot(Directory.GetCurrentDirectory())
+			//	.UseIISIntegration()
+			//	.UseUrls("http://*:5000")
+			//	//.UseKestrel((options) =>
+			//	//{
+			//	//	options.ThreadCount = 16;
+			//	//})
+			//	.UseStartup<Startup>()
+			//	.Build();
+
+			var contentRoot = Directory.GetCurrentDirectory();
+
+			//var pfxFile = @"E:\Users\Chris\Desktop\Topic\OpenSsl\cer\localhost2\localhost.pfx";
+			var pfxFile = Path.Combine(contentRoot, "ssl.pfx");
+			var certificate = new X509Certificate2(pfxFile);
+
 			var host = new WebHostBuilder()
 				.UseKestrel()
-				.UseContentRoot(Directory.GetCurrentDirectory())
+				.UseContentRoot(contentRoot)
 				.UseIISIntegration()
-				.UseUrls("http://*:5000")
-				//.UseKestrel((options) =>
-				//{
-				//	options.ThreadCount = 16;
-				//})
+				.UseUrls("https://*:5001", "http://*:5000")
+				.UseKestrel((options) =>
+				{
+					options.ThreadCount = 16;
+					options.UseHttps(certificate);
+				})
 				.UseStartup<Startup>()
 				.Build();
 
