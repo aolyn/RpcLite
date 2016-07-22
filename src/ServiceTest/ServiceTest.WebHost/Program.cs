36ad3561b50324124a2c17eafafe97d3dc77cafe
Chris <chrishwang@live.cn>
2016-07-22 15:15:40 +0800

    add ssl argument

--- src/ServiceTest/ServiceTest.WebHost/Program.cs
+++ src/ServiceTest/ServiceTest.WebHost/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Hosting;
 
@@ -8,38 +9,50 @@ namespace ServiceTest.WebHost
 	{
 		public static void Main(string[] args)
 		{
-			//var host = new WebHostBuilder()
-			//	.UseKestrel()
-			//	.UseContentRoot(Directory.GetCurrentDirectory())
-			//	.UseIISIntegration()
-			//	.UseUrls("http://*:5000")
-			//	//.UseKestrel((options) =>
-			//	//{
-			//	//	options.ThreadCount = 16;
-			//	//})
-			//	.UseStartup<Startup>()
-			//	.Build();
+			Console.WriteLine(args?.Length);
+			//Console.WriteLine(args[0]);
 
-			var contentRoot = Directory.GetCurrentDirectory();
+			var useHttps = args?.Length > 0 && args[0] == "ssl";
 
-			//var pfxFile = @"E:\Users\Chris\Desktop\Topic\OpenSsl\cer\localhost2\localhost.pfx";
-			var pfxFile = Path.Combine(contentRoot, "ssl.pfx");
-			var certificate = new X509Certificate2(pfxFile);
+			if (useHttps)
+			{
+				var contentRoot = Directory.GetCurrentDirectory();
 
-			var host = new WebHostBuilder()
-				.UseKestrel()
-				.UseContentRoot(contentRoot)
-				.UseIISIntegration()
-				.UseUrls("https://*:5001", "http://*:5000")
-				.UseKestrel((options) =>
-				{
-					options.ThreadCount = 16;
-					options.UseHttps(certificate);
-				})
-				.UseStartup<Startup>()
-				.Build();
+				//var pfxFile = @"E:\Users\Chris\Desktop\Topic\OpenSsl\cer\localhost2\localhost.pfx";
+				var pfxFile = Path.Combine(contentRoot, "ssl.pfx");
+				var certificate = new X509Certificate2(pfxFile);
 
-			host.Run();
+				var host = new WebHostBuilder()
+					.UseKestrel()
+					.UseContentRoot(contentRoot)
+					.UseIISIntegration()
+					.UseUrls("https://*:5001", "http://*:5000")
+					.UseKestrel((options) =>
+					{
+						options.ThreadCount = 16;
+						options.UseHttps(certificate);
+					})
+					.UseStartup<Startup>()
+					.Build();
+
+				host.Run();
+			}
+			else
+			{
+				var host = new WebHostBuilder()
+					.UseKestrel()
+					.UseContentRoot(Directory.GetCurrentDirectory())
+					.UseIISIntegration()
+					.UseUrls("http://*:5000")
+					//.UseKestrel((options) =>
+					//{
+					//	options.ThreadCount = 16;
+					//})
+					.UseStartup<Startup>()
+					.Build();
+
+				host.Run();
+			}
 		}
 	}
 }
